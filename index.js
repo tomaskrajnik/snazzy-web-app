d4c28057dcbefc0abe6f2a9bf06ed5e2ca01616d
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-24 16:58:04 +0200

    ssa

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 const express = require("express");
 const app = express();
 const logger = require("./startup/logger");
-const path = require("path");
 
 require("./startup/cors")(app);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
-} else {
-  app.use(express.static(path.join(__dirname, "/client/public")));
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
-  });
-}
-
 require("./startup/routes")(app);
 require("./startup/db")();
 // require("./startup/config");
--- startup/routes.js
+++ startup/routes.js
@@ -4,10 +4,24 @@ const register = require("./../routes/register");
 const login = require("./../routes/login");
 const profile = require("./../routes/profile");
 const plans = require("./../routes/plans");
+const path = require("path");
 
 module.exports = function (app) {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("/*", function (req, res) {
+      res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
+  } else {
+    app.use(express.static(path.join(__dirname, "/client/public")));
+    app.get("/*", function (req, res) {
+      res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    });
+  }
+
   app.use("/api/register", register);
   app.use("/api/login", login);
   app.use("/api/profile", profile);
