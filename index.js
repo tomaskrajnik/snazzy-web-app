7c16c42b43b569048fa31231e7805b62fc3608ff
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-24 23:31:50 +0200

    saknsgskd

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const path = require("path");
 
 require("./startup/cors")(app);
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
