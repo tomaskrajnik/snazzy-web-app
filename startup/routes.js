bf38a521d785a7b68fb64d66497b87d343404ac6
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-24 18:51:19 +0200

    node updates

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const error = require("./../middleware/error");
-const register = require("./../routes/register");
-const login = require("./../routes/login");
-const profile = require("./../routes/profile");
-const plans = require("./../routes/plans");
+// const register = require("./../routes/register");
+// const login = require("./../routes/login");
+// const profile = require("./../routes/profile");
+// const plans = require("./../routes/plans");
 
 module.exports = function (app) {
   app.use("/api/register", register);
