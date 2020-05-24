a7ce24864f9672d7a2958df4df14e112e6e43868
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-24 15:28:44 +0200

    db logs added3

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("./startup/cors")(app);
 app.use(express.static("client/build"));
 require("./startup/routes")(app);
 require("./startup/db")();
-require("./startup/config");
+require("./startup/config")();
 require("./startup/prod")(app);
 
 const port = process.env.PORT || 5000;
