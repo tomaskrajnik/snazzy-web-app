891d168451dea43e8a3e1524b66561095e380d41
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-24 12:00:48 +0200

    test

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const express = require("express");
 const app = express();
 const logger = require("./startup/logger");
 
-require("./startup/cors")(app);
+// require("./startup/cors")(app);
 app.use(express.static("client/build"));
-require("./startup/routes")(app);
-require("./startup/db")();
-require("./startup/config");
-require("./startup/prod")(app);
+// require("./startup/routes")(app);
+// require("./startup/db")();
+// require("./startup/config");
+// require("./startup/prod")(app);
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   logger.log.info(`Starting application on port ${port}`);
 });
 
-const path = require("path");
-if (process.env.NODE_ENV === "production") {
-  console.log("smth");
-  app.use("*", express.static(path.join(__dirname, "client", "build")));
-}
+// const path = require("path");
+// if (process.env.NODE_ENV === "production") {
+//   console.log("smth");
+//   app.use("*", express.static(path.join(__dirname, "client", "build")));
+// }
 
 module.exports = server;
