27fd68a39687daa4ba75ba7632b8201eafcadce9
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-24 17:27:29 +0200

    er

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ new Airbrake.Notifier({
   projectKey: "4830ba04a44285a222e351b2ceab91c5",
   environment: "production",
 });
-throw new Error("I am an uncaught exception");
+
 require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
@@ -17,6 +17,7 @@ require("./startup/prod")(app);
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
+  throw new Error("I am an uncaught exception");
   logger.log.info(`Starting application on port ${port}`);
 });
 
