82cdf7f167457c823902d51e7fc16cf1d5740d99
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-24 20:41:10 +0200

    asa

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "http://localhost:5000/api/",
   headers: {
-    "Access-Control-Allow-Origin": "*, Origin",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin": "Origin",
     "Content-Type": "application/json",
     "Access-Control-Allow-Headers":
       "x-requested-with, authorization, content-type, Origin",
