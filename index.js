c251d90ba513ac1cfa8aa8a9f168d754905dd609
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-24 13:48:24 +0200

    index.js refacture

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const express = require("express");
 const app = express();
 const logger = require("./startup/logger");
+const router = require("express").Router();
 
 const register = require("./routes/register");
 const login = require("./routes/login");
 const profile = require("./routes/profile");
 const plans = require("./routes/plans");
+const path = require("path");
 
 require("./startup/cors")(app);
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ADD THIS LINE
 app.use(express.static("client/build"));
-// app.use("/api/register", register);
-// app.use("/api/login", login);
-// app.use("/api/profile", profile);
-// app.use("/api/plans", plans);
+
+router.use("/api/register", register);
+router.use("/api/login", login);
+router.use("/api/profile", profile);
+router.use("/api/plans", plans);
 // require("./startup/routes")(app);
+
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 require("./startup/db")();
 require("./startup/config");
 require("./startup/prod")(app);
