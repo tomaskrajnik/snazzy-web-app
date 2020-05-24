6e6bf2192371f4a3010770d23fec9118b49c4f0d
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-24 23:01:34 +0200

    cors agaisn

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,10 +4,9 @@ const api = axios.create({
   baseURL: "http://localhost:5000/api/",
   headers: {
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Origin": "origin",
     "Content-Type": "application/json",
     "Access-Control-Allow-Headers":
-      "x-requested-with, authorization, content-type, Origin",
+      "x-requested-with, authorization, content-type",
     "Access-Control-Expose-Headers": "x-auth-token",
   },
 });
