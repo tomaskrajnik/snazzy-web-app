93fd8bf2f3dabbf6ed1be873e9fc3eb11dfbb2ec
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-24 20:14:02 +0200

    kasnkf

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,17 +1,14 @@
 import axios from "axios";
 
-const configHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  Accept: "application/json",
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Headers":
-    "x-requested-with, authorization, content-type",
-  "Access-Control-Expose-Headers": "x-auth-token",
-};
-
 const api = axios.create({
   baseURL: "http://localhost:5000/api/",
-  headers: configHeaders,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Headers":
+      "x-requested-with, authorization, content-type",
+    "Access-Control-Expose-Headers": "x-auth-token",
+  },
 });
 
 export default api;
