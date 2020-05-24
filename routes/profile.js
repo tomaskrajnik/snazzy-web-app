9a104fd1f1fac4c47456d96fd81ac7d868c54ecf
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-24 22:28:30 +0200

    removed smth

--- routes/profile.js
+++ routes/profile.js
@@ -4,16 +4,12 @@ const { User } = require("../models/user");
 const auth = require("./../middleware/auth");
 const validateObjectId = require("./../middleware/validateObjectId");
 
-router.get(
-  "/:id",
-  [auth, validateObjectId, cors(corsOptions)],
-  async (req, res) => {
-    const user = await User.findById(req.params.id).select("-password");
-    if (!user)
-      return res.status(404).send("User with the given id does not exist");
+router.get("/:id", [auth, validateObjectId], async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+  if (!user)
+    return res.status(404).send("User with the given id does not exist");
 
-    res.send(user);
-  }
-);
+  res.send(user);
+});
 
 module.exports = router;
