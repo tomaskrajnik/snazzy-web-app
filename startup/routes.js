5279ef431e96d3d527fd03198a205745b102ea2d
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-24 18:19:20 +0200

    sasfnaksa

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const error = require("./../middleware/error");
-// const register = require("./../routes/register");
+const register = require("./../routes/register");
 const login = require("./../routes/login");
 // const profile = require("./../routes/profile");
 // const plans = require("./../routes/plans");
