a1521a2e0bd52c74aa45a29b89fadd54101ada0b
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-24 12:15:32 +0200

    removed routes test

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const logger = require("./startup/logger");
 
 // require("./startup/cors")(app);
 app.use(express.static("client/build"));
-require("./startup/routes")(app);
-// require("./startup/db")();
-// require("./startup/config");
-// require("./startup/prod")(app);
+// require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config");
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
