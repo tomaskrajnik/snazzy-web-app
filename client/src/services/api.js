d0c036db99d3dc279a7b50ce568fd624761d446f
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-25 07:46:04 +0200

    aksnfa

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "http://localhost:5000/api/",
   headers: {
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin": "http://localhost:5000/api/",
     "Content-Type": "application/json",
     "Access-Control-Allow-Headers":
       "x-requested-with, authorization, content-type",
