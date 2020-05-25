4914605f8f543e5e4e44a92eb1cd32836252989c
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-05-25 18:16:10 +0200

    kansf

--- middleware/error.js
+++ middleware/error.js
@@ -1,4 +1,6 @@
+const logger = require("./../startup/logger");
 module.exports = function (err, req, res, next) {
   console.log(err.message, err);
+  logger.log.error(err.message, err);
   res.status(500).send("Something went wrong.");
 };
