dc20766b54696e9ed9d94294d08e313ce4a2217f
Tomas Krajnik <tomas.krajnik@gmail.com>
2020-06-10 20:34:11 +0200

    changed cors to prepare for deployment on staging

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "/api/",
   headers: {
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin": "http://localhost:5000",
     "Content-Type": "application/json",
     "Access-Control-Allow-Headers":
       "x-requested-with, authorization, content-type",
